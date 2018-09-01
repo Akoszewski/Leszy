6c96de0eea839678df8330da1d18b4dd4b63218f
Jeschyk <33879805+Jeschyk@users.noreply.github.com>
2018-09-01 16:58:44 +0200

    Zmiana jezyka kalendarza na polski i ustawienie pierwszego dnia tygodnia na poniedzialek

--- app/src/main/java/com/example/user/Leszy/CalendarActivity.java
+++ app/src/main/java/com/example/user/Leszy/CalendarActivity.java
@@ -1,21 +1,37 @@
 package com.example.user.Leszy;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CalendarView;
 
+import java.util.Locale;
+
 public class CalendarActivity extends AppCompatActivity{
 
     private static final String TAG="CalendarActivity";
 
-    @Override
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_layout);
+        CalendarView simpleCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        simpleCalendarView.setFirstDayOfWeek(2);
+
+        String languageToLoad  = "pl";
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+        this.setContentView(R.layout.calendar_layout);
+
     }
+
     public void back1(View view) {
         onBackPressed();
     }
